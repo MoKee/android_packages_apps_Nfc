5af0c0568aa4aa9a03986de35d0b609ef2139e80
XiNGRZ <chenxingyu92@gmail.com>
2019-10-15 14:06:04 +0800

    NFC: nfcid: Implement ID emulation
    Change-Id: Ia5e647833f40906c9db7af7f344e920d71b1c840

--- Android.mk
+++ Android.mk
@@ -13,12 +13,16 @@ LOCAL_SRC_FILES := \
 LOCAL_SRC_FILES += \
         $(call all-java-files-under, nxp)
 
+LOCAL_SRC_FILES += \
+        $(call all-java-files-under, nfcid)
+
 LOCAL_PACKAGE_NAME := Nfc
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 
 LOCAL_JNI_SHARED_LIBRARIES  := libnfc_jni
+LOCAL_JNI_SHARED_LIBRARIES  += libmokee_nfc_id_jni
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
@@ -39,6 +43,9 @@ LOCAL_SRC_FILES := \
 LOCAL_SRC_FILES += \
         $(call all-java-files-under, nci)
 
+LOCAL_SRC_FILES += \
+        $(call all-java-files-under, nfcid)
+
 LOCAL_PACKAGE_NAME := NfcNci
 LOCAL_OVERRIDES_PACKAGES := Nfc
 
@@ -46,6 +53,7 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 
 LOCAL_JNI_SHARED_LIBRARIES := libnfc_nci_jni
+LOCAL_JNI_SHARED_LIBRARIES += libmokee_nfc_id_jni
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
